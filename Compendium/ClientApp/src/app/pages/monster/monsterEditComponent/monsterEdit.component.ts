8a71abae1cc33ad408c9b140a1bb6421c9e5c8b0
Genae07 <stefan-stark@outlook.com>
2023-02-06 23:07:27 +0100

    cleanup

--- Compendium/ClientApp/src/app/pages/monster/monsterEditComponent/monsterEdit.component.ts
+++ Compendium/ClientApp/src/app/pages/monster/monsterEditComponent/monsterEdit.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {
     Monster,
@@ -7,11 +7,8 @@ import {
     MonsterType,
     Ability,
     ChallengeRating,
-    ArmorGroup,
-    ArmorPiece,
     DamageType,
     ArmorInfo,
-    Condition,
     Morality,
     Order,
     Multiattack,
@@ -24,16 +21,17 @@ import {
     Trait
 } from "../../../models/monster";
 import { Spell } from "../../../models/spell";
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { TextgenService } from '../../../services/textgen.service';
 import { WeaponCategory, WeaponType } from '../../../models/weapon';
 import { MonsterService } from '../../../services/monster.service';
 import { SpellService } from '../../../services/spell.service';
 import { WeaponTypeService } from '../../../services/weaponType.service';
 import { Project } from '../../../models/project';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl} from "@angular/forms";
 import {StatsByCr, StatsByCrList} from "../../../models/lists/statsByCrList";
 import {HitDieSize, HitDieSizeList} from "../../../models/lists/hitDieSizeList";
+import {CRList} from "../../../models/lists/CRList";
 
 @Component({
     selector: 'monsterEdit',
@@ -337,73 +335,12 @@ export class MonsterEditComponent implements OnInit {
 
     public sizeValues = Object.values(Size);
     public dmgTypeValues = Object.values(DamageType);
-    public conditionValues = Object.values(Condition);
     public monsterTypeValues = Object.values(MonsterType);
     public atkTypeValues = Object.values(AttackType);
 
-    public vulDesc(vul: DamageType[] | string[]): string {
-        if (vul === undefined)
-            return "";
-        return vul.map(v => {v + ""}).join(", ")
-    }
 
 
-    public crValues: ChallengeRating[] = [
-        { value: -3, experience: 0, description: "0 (0 XP)" },
-        { value: -3, experience: 10, description: "0 (10 XP)" },
-        { value: -2, experience: 25, description: "1/8 (25 XP)" },
-        { value: -1, experience: 50, description: "1/4 (50 XP)" },
-        { value: 0, experience: 100, description: "1/2 (100 XP)" },
-        { value: 1, experience: 200, description: "1 (200 XP)" },
-        { value: 2, experience: 450, description: "2 (450 XP)" },
-        { value: 3, experience: 700, description: "3 (700 XP)" },
-        { value: 4, experience: 1100, description: "4 (1.100 XP)" },
-        { value: 5, experience: 1800, description: "5 (1.800 XP)" },
-        { value: 6, experience: 2300, description: "6 (2.300 XP)" },
-        { value: 7, experience: 2900, description: "7 (2.900 XP)" },
-        { value: 8, experience: 3900, description: "8 (3.900 XP)" },
-        { value: 9, experience: 5000, description: "9 (5.000 XP)" },
-        { value: 10, experience: 5900, description: "10 (5.900 XP)" },
-        { value: 11, experience: 7200, description: "11 (7.200 XP)" },
-        { value: 12, experience: 8400, description: "12 (8.400 XP)" },
-        { value: 13, experience: 10000, description: "13 (10.000 XP)" },
-        { value: 14, experience: 11500, description: "14 (11.500 XP)" },
-        { value: 15, experience: 13000, description: "15 (13.000 XP)" },
-        { value: 16, experience: 15000, description: "16 (15.000 XP)" },
-        { value: 17, experience: 18000, description: "17 (18.000 XP)" },
-        { value: 18, experience: 20000, description: "18 (20.000 XP)" },
-        { value: 19, experience: 22000, description: "19 (22.000 XP)" },
-        { value: 20, experience: 25000, description: "20 (25.000 XP)" },
-        { value: 21, experience: 33000, description: "21 (33.000 XP)" },
-        { value: 22, experience: 41000, description: "22 (41.000 XP)" },
-        { value: 23, experience: 50000, description: "23 (50.000 XP)" },
-        { value: 24, experience: 62000, description: "24 (62.000 XP)" },
-        { value: 25, experience: 75000, description: "25 (75.000 XP)" },
-        { value: 26, experience: 90000, description: "26 (90.000 XP)" },
-        { value: 27, experience: 105000, description: "27 (105.000 XP)" },
-        { value: 28, experience: 120000, description: "28 (120.000 XP)" },
-        { value: 29, experience: 135000, description: "29 (135.000 XP)" },
-        { value: 30, experience: 155000, description: "30 (155.000 XP)" },
-        { value: 31, experience: 175000, description: "31 (175.000 XP)" },
-        { value: 32, experience: 195000, description: "32 (195.000 XP)" },
-        { value: 33, experience: 215000, description: "33 (215.000 XP)" },
-        { value: 34, experience: 240000, description: "34 (240.000 XP)" },
-        { value: 35, experience: 265000, description: "35 (265.000 XP)" },
-        { value: 36, experience: 290000, description: "36 (290.000 XP)" },
-        { value: 37, experience: 315000, description: "37 (315.000 XP)" },
-        { value: 38, experience: 345000, description: "38 (345.000 XP)" },
-        { value: 39, experience: 375000, description: "39 (375.000 XP)" },
-        { value: 40, experience: 405000, description: "40 (405.000 XP)" },
-        { value: 41, experience: 435000, description: "41 (435.000 XP)" },
-        { value: 42, experience: 475000, description: "42 (475.000 XP)" },
-        { value: 43, experience: 515000, description: "43 (515.000 XP)" },
-        { value: 44, experience: 555000, description: "44 (555.000 XP)" },
-        { value: 45, experience: 595000, description: "45 (595.000 XP)" },
-        { value: 46, experience: 635000, description: "46 (635.000 XP)" },
-        { value: 47, experience: 685000, description: "47 (685.000 XP)" },
-        { value: 48, experience: 735000, description: "48 (735.000 XP)" },
-        { value: 49, experience: 835000, description: "49 (835.000 XP)" },
-    ];
+    public crValues: ChallengeRating[] = CRList.list;
     hitDieSize: HitDieSize[] = HitDieSizeList.list;
 
     sizeChanged(size: number) {
